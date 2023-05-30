492cdccd77d79c16b2f65bb8ac28382b3a7eb2bf
Remy Siandja <remysiandja@gmail.com>
2023-05-30 18:18:42 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import '@/styles/globals.css'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import "../public/css/style.css";
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
